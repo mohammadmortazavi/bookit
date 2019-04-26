1a9f46a9d6347dca3507be908e7a7988c9aae393
MVC-core <MVC@MVC-PC>
2019-04-26 18:29:52 +0430

    DbbookTale

---
+++ bookit/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bookit.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Descrption { get; set; }
+        public int Price { get; set; }
+    }
+}

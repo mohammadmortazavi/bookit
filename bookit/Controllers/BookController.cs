afb8e584c3b8aa69305fb319fb1b65d66dfa4089
MVC-core <MVC@MVC-PC>
2019-05-08 18:14:57 +0430

    bb

---
+++ bookit/Controllers/BookController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using bookit.Data;
+using bookit.Models;
+
+namespace bookit.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public BookController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var model = _db.Books;
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Book model)
+        {
+            _db.Books.Add(model);
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Details(int id)
+        {
+            var model = _db.Books.Find(id);
+                return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var model = _db.Books.Find(id);
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Book model)
+        {
+            _db.Entry(model).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            _db.SaveChanges();
+            
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var model = _db.Books.Find(id);
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Book model)
+        {
+            _db.Entry(model).State = Microsoft.EntityFrameworkCore.EntityState.Deleted;
+            _db.SaveChanges();
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
